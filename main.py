72e4eb26b44f09545e263dbf8f7b318abccf985a
vaultboy212 <vaultboy21@icloud.com>
2026-02-16 10:06:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen. \n\n More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...")
+from transliterate import translit
+from num2words import num2words
+
+text = "Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.\n\nMore than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8..."
+
+# текст
+print(translit(text, "ru"))
+print()
+
+# числа отдельно
+print("78 -", translit(num2words(78), "ru"))
+print("15 -", translit(num2words(15), "ru"))
+print("3 -", translit(num2words(3), "ru"))
+print("40 -", translit(num2words(40), "ru"))
+print("8 -", translit(num2words(8), "ru"))
